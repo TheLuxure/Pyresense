3c0cdcc0d9347624fad57bfa02dc68853c51210f
TheLuxure <65787578+TheLuxure@users.noreply.github.com>
2020-05-27 16:57:54 +0500

    Alchemist NPC

---
+++ NPCs/Town/Alchemist.cs
@@ -0,0 +1,264 @@
+using System;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace Pyresense.NPCs.Town
+{
+	// [AutoloadHead] and npc.townNPC are extremely important and absolutely both necessary for any Town NPC to work at all.
+	[AutoloadHead]
+	public class Alchemist : ModNPC
+	{
+		public override string Texture => "Pyresense/NPCs/Town/Alchemist";
+
+		//public override string[] AltTextures => new[] { "Pyresense/NPCs/Alchemist_Alt_1" };
+
+		public override bool Autoload(ref string name) {
+			name = "Alchemist";
+			return mod.Properties.Autoload;
+		}
+
+		public override void SetStaticDefaults() {
+			// DisplayName automatically assigned from .lang files, but the commented line below is the normal approach.
+			// DisplayName.SetDefault("Example Person");
+			Main.npcFrameCount[npc.type] = 25;
+			NPCID.Sets.ExtraFramesCount[npc.type] = 9;
+			NPCID.Sets.AttackFrameCount[npc.type] = 4;
+			NPCID.Sets.DangerDetectRange[npc.type] = 700;
+			NPCID.Sets.AttackType[npc.type] = 0;
+			NPCID.Sets.AttackTime[npc.type] = 90;
+			NPCID.Sets.AttackAverageChance[npc.type] = 30;
+			NPCID.Sets.HatOffsetY[npc.type] = 4;
+		}
+
+		public override void SetDefaults() {
+			npc.townNPC = true;
+			npc.friendly = true;
+			npc.width = 18;
+			npc.height = 40;
+			npc.aiStyle = 7;
+			npc.damage = 10;
+			npc.defense = 15;
+			npc.lifeMax = 250;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath1;
+			npc.knockBackResist = 0.5f;
+			animationType = NPCID.Guide;
+		}
+
+		public override bool CanTownNPCSpawn(int numTownNPCs, int money) {
+			for (int k = 0; k < 255; k++) {
+				Player player = Main.player[k];
+				if (!player.active) {
+					continue;
+				}
+
+				foreach (Item item in player.inventory) {
+					if (item.type == 2344 || item.type == 303 || item.type == 300 || item.type == 2325 || item.type == 2324 || item.type == 2326 ||
+						item.type == 2329 || item.type == 2346 || item.type == 295 || item.type == 2354 || item.type == 2327 || item.type == 291 ||
+						item.type == 305 || item.type == 2326 || item.type == 2323 || item.type == 304 || item.type == 2348 || item.type == 297 ||
+						item.type == 292 || item.type == 2345 || item.type == 2352 || item.type == 294 || item.type == 293 || item.type == 2322 ||
+						item.type == 299 || item.type == 288 || item.type == 2347 || item.type == 289 || item.type == 298 || item.type == 2355 ||
+						item.type == 296 || item.type == 2353 || item.type == 2328 || item.type == 290 || item.type == 301 || item.type == 2326 ||
+						item.type == 2359 || item.type == 302 || item.type == 2349) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public override string TownNPCName() {
+			switch (WorldGen.genRand.Next(5)) {
+				case 0:
+					return "Mol";
+				case 1:
+					return "Vin";
+				case 2:
+					return "Van";
+				case 3:
+					return "Mist";
+				default:
+					return "Alchem";
+			}
+		}
+
+		public override void FindFrame(int frameHeight) {
+			/*npc.frame.Width = 40;
+			if (((int)Main.time / 10) % 2 == 0)
+			{
+				npc.frame.X = 40;
+			}
+			else
+			{
+				npc.frame.X = 0;
+			}*/
+		}
+
+		public override string GetChat() {
+			switch (Main.rand.Next(2)) {
+				case 0:
+					return "...";
+				default:
+					return "... What?";
+			}
+		}
+
+		public override void SetChatButtons(ref string button, ref string button2) {
+			button = Language.GetTextValue("LegacyInterface.4");
+		}
+		public override void OnChatButtonClicked(bool firstButton, ref bool shop)
+		{
+			if (firstButton)
+			{
+				shop = true;
+			}
+		}
+		public override void SetupShop(Chest shop, ref int nextSlot) {
+			shop.item[nextSlot].SetDefaults(2344);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(303);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(300);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2325);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2324);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2326);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2329);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2346);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(295);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2354);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2327);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(291);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(305);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2326);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2323);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(304);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2348);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(297);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(292);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2345);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2352);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(294);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(293);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2322);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(299);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(288);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2347);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(289);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(298);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2355);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(296);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2353);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2328);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(290);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(301);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2326);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2359);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(302);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+			shop.item[nextSlot].SetDefaults(2349);
+			shop.item[nextSlot].shopCustomPrice = 4000;
+			nextSlot++;
+		}
+
+		public override bool CanGoToStatue(bool toKingStatue) {
+			return true;
+		}
+
+		public override void TownNPCAttackStrength(ref int damage, ref float knockback) {
+			damage = 20;
+			knockback = 4f;
+		}
+
+		public override void TownNPCAttackCooldown(ref int cooldown, ref int randExtraCooldown) {
+			cooldown = 30;
+			randExtraCooldown = 30;
+		}
+
+		public override void TownNPCAttackProj(ref int projType, ref int attackDelay) {
+			projType = 45;
+			attackDelay = 1;
+		}
+
+		public override void TownNPCAttackProjSpeed(ref float multiplier, ref float gravityCorrection, ref float randomOffset) {
+			multiplier = 12f;
+			randomOffset = 2f;
+		}
+	}
+}
